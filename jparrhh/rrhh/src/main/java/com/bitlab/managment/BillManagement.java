59b7f881b7ac5236048b284a263668686c793f1f
juan carlos ruiz nativi <juanativi95@gmail.com>
2020-07-30 01:16:08 -0600

    30/07/2020 BillManagement
    se agrega el primer managemenet

---
+++ jparrhh/rrhh/src/main/java/com/bitlab/managment/BillManagement.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bitlab.managment;
+
+import com.bitlab.dao.AbstractDao;
+import com.bitlab.dao.BillDao;
+import com.bitlab.entities.Bill;
+
+/**
+ *
+ * @author juana
+ */
+public class BillManagement extends AbstractManagment<Bill> {
+
+    private BillDao billDao;
+
+    public BillManagement() {
+        super(Bill.class);
+        billDao = new BillDao();
+
+    }
+
+    @Override
+    public AbstractDao<Bill> getControler() {
+        return billDao;
+    }
+
+}
