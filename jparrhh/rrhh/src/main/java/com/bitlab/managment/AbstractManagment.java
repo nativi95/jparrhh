9d1db634d0263419ecd7e2a4a40def51902cf7da
juan carlos ruiz nativi <juanativi95@gmail.com>
2020-07-30 01:11:01 -0600

    7/30/2020 AbstractManagement
    se agrega el controlador Managment

---
+++ jparrhh/rrhh/src/main/java/com/bitlab/managment/AbstractManagment.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bitlab.managment;
+
+import com.bitlab.dao.AbstractDao;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ *
+ * @author juana
+ */
+public abstract class AbstractManagment<T> {
+    
+    private T entitySelected;
+    private List<T> entities;
+    private Class<T> EntityClass;
+
+    public AbstractManagment(Class<T> entity) {
+        EntityClass = entity;
+    }
+    
+    @PostConstruct
+    public void init(){
+    entities= getControler().findAll();
+    }
+    
+    @PreDestroy
+    public void destroit(){
+    entitySelected = null;
+    entities = null;
+    EntityClass = null;
+    }
+    
+    public abstract AbstractDao<T> getControler();
+}
